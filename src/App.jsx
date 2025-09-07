4b0a770ce4931a70224e38838de7670f264d78b0
BAZIZ Abdelhalim <baziz.halim94@gmail.com>
2025-09-07 14:15:34 +0200

    callback

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,59 @@
-import { useState } from 'react'
+import { useState, useEffect , memo} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { useFetch } from './hooks/useFetch'
+
 
 
 function App() {
-
-  const {loading, data, errors} = useFetch('https://jsonplaceholder.typicode.com/posts?_limit=10&_delay=5000')
-
-  return <div className='container my-2 fixed-top'>
-
-            {loading && <div className='spinner-border' role="status"> 
-
-                            <span className='visually-hidden'> Loading...</span>
-
-                        </div>}
-            
-            {errors && <div className='alert alert-danger'>{errors.toString()}</div>}
-            {data && <div>
-                        <ul>
-                            {data.map(article => (<li key={article.id}> {article.title} </li>))}
-                        </ul>
-
-                     </div>}
   
+  const [name, setName] = useState('') 
+        console.log("APP")
+
+
+  return <div className='container my-2 vstack gap-2 fixed-top'>
+    <div>
+      <label>Prénom</label>
+      <Input value={name} onSetChange={setName} placeholder="Tape ton prénom" />
+      <div>
+        {name.toUpperCase()}
+      </div>
+    </div>
+    <InfoMemo/>
   </div>
 }
+
+function Input({ value, onSetChange, placeholder = "Écrire ici..." }) {
+  return (
+    <input
+      type="text"
+      className="form-control"
+      value={value}
+      placeholder={placeholder}
+      onChange={(e) => onSetChange(e.target.value)}
+    />
+  );
+}
   
+
+
+const InfoMemo = memo(function Info(){
+
+   useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(true)
+    }, 500) // attend 2 secondes
+
+    return () => clearTimeout(timer) // nettoyage si le composant se démonte
+  }, [])
+
+  console.log("info")
+  
+return <div className='alert alert-info'>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae provident quo reiciendis iste iure distinctio ratione voluptatem voluptas ea quibusdam debitis odio repudiandae, ut quaerat amet totam recusandae itaque corrupti!
+  </div>
+})
+
+
 
 
 export default App
