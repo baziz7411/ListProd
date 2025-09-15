9791546a265c950d4b8ffacffbb44fa335f83b31
BAZIZ Abdelhalim <baziz.halim94@gmail.com>
2025-09-15 10:59:50 +0200

    reducer

--- src/App.jsx
+++ src/App.jsx
@@ -6,75 +6,52 @@ import React from 'react'
 
 
 
-const initialState = {
-  todos: [
-    { name: 'Faire les courses', checked: false },
-    { name: 'Ranger les courses', checked: false },
-    { name: 'Manger les courses', checked: false },
-  ],
-  currentIndex: -1, // rien sélectionné au départ
-}
-
 function reducer(state, action) {
   switch (action.type) {
-    case "NEXT":
-      if (state.currentIndex < state.todos.length - 1) {
-        const newIndex = state.currentIndex + 1;
-        const updated = state.todos.map((todo, i) =>
-          i === newIndex ? { ...todo, checked: true } : todo
-        );
-        return { ...state, todos: updated, currentIndex: newIndex };
+    case 'REMOVE_TODO': {
+      return {
+          todos: state.todo.filter(t => t !== action.todo)
+      };
+    };
+    case 'ADD_TODO': {
+      return {
+        todos: [{
+          id: state.todos.length,
+          text: action.text
+        }, ...state.todos]
       }
-      return state;
-
-    case "PREV":
-      if (state.currentIndex >= 0) {
-        const newIndex = state.currentIndex;
-        const updated = state.todos.map((todo, i) =>
-          i === newIndex ? { ...todo, checked: false } : todo
-        );
-        return { ...state, todos: updated, currentIndex: newIndex - 1 };
-      }
-      return state;
-
-    default:
-      return state;
+    }
   }
+  throw Error('Action non reconnue: ' + action.type);
 }
+
 
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, {
+        todos: [{
+            name : "Faire les courses",
+            checked : false
+        },
+        {
+            name :"Ranger les course",
+            checked : false
+        },
+        {
+            name : "Manger les course",
+            checked : false
+        }]
 
-  return (
-    <div className="container my-3">
-      <ul className="list-group list-group-horizontal">
-        {state.todos.map((todo, i) => (
-          <li
-            key={todo.name}
-            className={`list-group-item ${todo.checked ? "bg-success text-white" : ""}`}
-          >
-            {todo.name}
-          </li>
-        ))}
-      </ul>
+  })
 
-      <div className="mt-3 d-flex gap-2">
-        <button
-          className="btn btn-primary"
-          onClick={() => dispatch({ type: "PREV" })}
-        >
-          Précédent
-        </button>
-        <button
-          className="btn btn-success"
-          onClick={() => dispatch({ type: "NEXT" })}
-        >
-          Suivant
-        </button>
-      </div>
-    </div>
-  );
+  return <div className="container fixed-top">
+            <ul>
+                {state.todos.map(todo => (<li key ={todo.name}>{todo.name} <button>supprimer</button></li>))}
+            </ul>
+
+
+  </div>
+    
 }
 
 export default App
